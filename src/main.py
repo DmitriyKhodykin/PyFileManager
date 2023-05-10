31206421d4255fd0063705d9e0b471680b901e51
Dmitry.Khodykin <dmitry.hodykin@itentika.ru>
2023-05-10 10:33:12 +0300

    feat(main): Create startup point

--- src/main.py
+++ src/main.py
@@ -2,3 +2,17 @@
 Содержит основной код приложения и точку входа в программу.
 """
 
+import sys
+from PyQt6.QtWidgets import QApplication
+from file_manager.file_explorer import FileExplorer
+
+
+def main():
+    app = QApplication(sys.argv)
+    file_explorer = FileExplorer()
+    file_explorer.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
